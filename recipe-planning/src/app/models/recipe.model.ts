e203a46a28927b3e1e1cdfa2c7724ae995455a55
DELL <DELL@DESKTOP-OVDL1DT>
2025-04-22 22:16:55 +0200

    commit

--- recipe-planning/src/app/models/recipe.model.ts
+++ recipe-planning/src/app/models/recipe.model.ts
@@ -1,10 +1,12 @@
-export interface Recipe {
-  id?: string;
-  recipe_name: string;
-  ingredients: string[];
-  instructions: string[];
-  nutrition_facts: string;
-  image_path: string;
-  author: string;
-  created_at?: Date;
+export class Recipe {
+  constructor(
+    public id: string,
+    public name: string,
+    public imagePath: string,
+    public ingredients: [],
+    public instructions: [],
+    public type: string,
+    public author: string,
+    public nutrition: string
+  ) {}
 }
